d93a0e3a667395d0d28bb64de25e27eafa49efc5
qiraxyz <ariqaziz74@gmail.com>
2023-10-01 23:47:56 +0700

    relocate feat: Model Data

---
+++ app/Models/User.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Laramus\Liberius\Models;
+
+/**
+ * Class User
+ * 
+ * Model for class User
+ */
+class User
+{
+    private $db;
+
+    public function __construct($db)
+    {
+        $this->db = $db;
+    }
+
+    /**
+     * 
+     * @return array
+     */
+}
